2db6c9b086e69d5464826267597f3b9d87053d1a
Simon Elvery <simon@elvery.net>
2013-10-03 21:19:05 +1000

    Remove comments to try and fix encoding issue

--- modules/php/manifests/params.pp
+++ modules/php/manifests/params.pp
@@ -1,17 +1,3 @@
-# Class: php::params
-#
-# This class defines default parameters used by the main module class php
-# Operating Systems differences in names and paths are addressed here
-#
-# == Variables
-#
-# Refer to php class for the variables defined here.
-#
-# == Usage
-#
-# This class is not intended to be used directly.
-# It may be imported or inherited by other classes
-#
 class php::params {
 
   $package_devel = $::operatingsystem ? {
