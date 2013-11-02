24a8b2f1c262df9c374d316603df9a931152b6a3
Simon Elvery <simon@elvery.net>
2013-11-02 15:22:19 +1000

    New Puppet config (less private config vars)

--- modules/java/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# Manage a Java installation
-class java {
-
-  # OpenJDK because Oracle does not allow repackaging the Oracle JDK into debs.
-  package {
-    'openjdk-6-jdk': ensure => present;
-  }
-}
